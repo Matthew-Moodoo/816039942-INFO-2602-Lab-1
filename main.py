b7cfc265678212d342d2d7b003a23b8bed9f5c07
Matthew-Moodoo <157634803+Matthew-Moodoo@users.noreply.github.com>
2025-01-31 03:57:28 +0000

    First commit

--- main.py
+++ main.py
@@ -14,4 +14,8 @@ with open('data.json') as f:
 def hello_world():
     return 'Hello, World!'  # return 'Hello World' in response
 
+@app.route('/students')
+def get_students():
+    return jsonify(data)# return student data in response
+
 app.run(host='0.0.0.0', port=8080, debug=True)
