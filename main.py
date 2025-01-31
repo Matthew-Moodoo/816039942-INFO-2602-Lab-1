a8422bd367485396d1c9909c52a553bb487a2c24
Matthew-Moodoo <157634803+Matthew-Moodoo@users.noreply.github.com>
2025-01-31 05:15:53 +0000

    Third Commit

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ def hello_world():
 
 @app.route('/students')
 def get_students():
-    return jsonify(data)# return student data in response
-
-# route variables
-@app.route('/students/<id>')
-def get_student(id):
-    for student in data:
-        if student['id'] == id: # filter out the students without the specified id
-            return jsonify(student)
+    result = []
+    pref = request.args.get('pref') # get the parameter from url
+    if pref:
+        for student in data: # iterate dataset
+            if student['pref'] == pref: # select only the students with a given meal preference
+                result.append(student) # add match student to the result
+        return jsonify(result) # return filtered set if parameter is supplied
+    return jsonify(data) # return entire dataset if no parameter supplied
 
 app.run(host='0.0.0.0', port=8080, debug=True)
