b6926aefc1d41d656fe24129514a1773547e9e1c
Matthew-Moodoo <157634803+Matthew-Moodoo@users.noreply.github.com>
2025-01-31 05:05:54 +0000

    Second commit

--- main.py
+++ main.py
@@ -18,4 +18,11 @@ def hello_world():
 def get_students():
     return jsonify(data)# return student data in response
 
+# route variables
+@app.route('/students/<id>')
+def get_student(id):
+    for student in data:
+        if student['id'] == id: # filter out the students without the specified id
+            return jsonify(student)
+
 app.run(host='0.0.0.0', port=8080, debug=True)
